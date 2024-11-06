fd892b60ce47ff016cac3bea7a30d089dde97c57
Raquel Torres <raqueltorresiglesias@gmail.com>
2024-11-06 06:54:31 +0100

    CodiR

---
+++ CodiR.R
@@ -0,0 +1,16 @@
+library(SummarizedExperiment)
+Dades_PAC1<-read.csv("C:/Users/Raque/Desktop/Modul B -Boring/UOC/Anàlisi Dades Òmiques/DataValues_S013.csv")
+metadades_PAC1<-Dades_PAC1[,1:10]
+dades_metabolits<-Dades_PAC1[,11:ncol(Dades_PAC1)]
+rownames(metadades_PAC1)<-as.character(metadades_PAC1[,1])
+metadades<-metadades_PAC1[,-1]
+colData<-DataFrame(metadades)
+dades_metabolits_matrix <- t(as.matrix(dades_metabolits))
+SumExp_PAC1<-SummarizedExperiment(assays = list(counts=dades_metabolits_matrix),
+                                  colData = colData)
+SumExp_PAC1
+ages<-colData(SumExp_PAC1)$AGE
+groups<-colData(SumExp_PAC1)$Group
+tapply(ages, groups, mean)
+tapply(ages, groups, sd)
+t.test(AGE ~ Group, data = colData)
